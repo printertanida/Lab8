72452a31a34cac2b2d1f92b2298e6c917f04f494
Tanida <printer.rung@gmail.com>
2024-12-06 16:48:33 +0700

    lab_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int age;
+    int height;
+    int bounty;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100){
+            cout << "Your character = Chopper";
+        }
+        else if(height < 180){
+            cout << "Your character = Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty>1100000000){
+                cout << "Your character = Zoro";
+            }
+            else{
+                cout << "Your character = Sanji";
+            }
+        }
+        
+    }
+    else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000){
+            cout << "Your character = Jinbe";
+        }
+        else{
+            cout << "Your character = Franky";
+        }
+        
+    }
+    else{
+        cout << "Your character = Brook";
+        
+    }
 /*
 "Enter your age: "
 "Enter your height: "
@@ -11,3 +58,8 @@
 "Brook"
 "Jinbe"
 */
+
+
+
+return 0;
+}
